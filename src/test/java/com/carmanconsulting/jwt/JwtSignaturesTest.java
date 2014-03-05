cad4f953e957108904d0724cac745bf1cb06b664
James Carman <jcarman@apache.org>
2014-03-05 13:55:20 -0500

    Cleaning up a bit.

--- src/test/java/com/carmanconsulting/jwt/JwtSignaturesTest.java
+++ src/test/java/com/carmanconsulting/jwt/JwtSignaturesTest.java
@@ -1,5 +1,6 @@
 package com.carmanconsulting.jwt;
 
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSObject;
@@ -15,8 +16,10 @@ import org.junit.Test;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -29,26 +32,9 @@ public class JwtSignaturesTest {
 // Other Methods
 //----------------------------------------------------------------------------------------------------------------------
 
-    private JWTClaimsSet createJwt() {
-        JWTClaimsSet jwtClaims = new JWTClaimsSet();
-        jwtClaims.setIssuer("https://sso.myco.com");
-        jwtClaims.setSubject("alice");
-        List<String> aud = new ArrayList<String>();
-        aud.add("https://app-one.com");
-        aud.add("https://app-two.com");
-        jwtClaims.setAudience(aud);
-        jwtClaims.setExpirationTime(new Date(new Date().getTime() + 1000 * 60 * 10));
-        jwtClaims.setNotBeforeTime(new Date());
-        jwtClaims.setIssueTime(new Date());
-        jwtClaims.setJWTID(UUID.randomUUID().toString());
-        return jwtClaims;
-    }
-
     @Test
     public void testRsaSignature() throws Exception {
-        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
-        keyGenerator.initialize(1024);
-        KeyPair kp = keyGenerator.genKeyPair();
+        KeyPair kp = createKeyPair();
         RSAPublicKey publicKey = (RSAPublicKey) kp.getPublic();
         RSAPrivateKey privateKey = (RSAPrivateKey) kp.getPrivate();
 
@@ -68,13 +54,37 @@ public class JwtSignaturesTest {
         String s = jwsObject.serialize();
 
         jwsObject = JWSObject.parse(s);
-        key = (RSAKey) jwsObject.getHeader().getJWK();
 
+        assertTrue(verify(jwsObject));
+
+        JWTClaimsSet copy = JWTClaimsSet.parse(jwsObject.getPayload().toJSONObject());
+        assertEquals(jwt.getJWTID(), copy.getJWTID());
+    }
+
+    private KeyPair createKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
+        keyGenerator.initialize(1024);
+        return keyGenerator.genKeyPair();
+    }
+
+    private JWTClaimsSet createJwt() {
+        JWTClaimsSet jwtClaims = new JWTClaimsSet();
+        jwtClaims.setIssuer("https://sso.myco.com");
+        jwtClaims.setSubject("alice");
+        List<String> aud = new ArrayList<String>();
+        aud.add("https://app-one.com");
+        aud.add("https://app-two.com");
+        jwtClaims.setAudience(aud);
+        jwtClaims.setExpirationTime(new Date(new Date().getTime() + 1000 * 60 * 10));
+        jwtClaims.setNotBeforeTime(new Date());
+        jwtClaims.setIssueTime(new Date());
+        jwtClaims.setJWTID(UUID.randomUUID().toString());
+        return jwtClaims;
+    }
+
+    private boolean verify(JWSObject jwsObject) throws NoSuchAlgorithmException, InvalidKeySpecException, JOSEException {
+        RSAKey key = (RSAKey) jwsObject.getHeader().getJWK();
         JWSVerifier verifier = new RSASSAVerifier(key.toRSAPublicKey());
-
-        assertTrue(jwsObject.verify(verifier));
-
-        JWTClaimsSet deserialized = JWTClaimsSet.parse(jwsObject.getPayload().toJSONObject());
-        assertEquals(jwt.getJWTID(), deserialized.getJWTID());
+        return jwsObject.verify(verifier);
     }
 }
